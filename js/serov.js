3465e5f9e36ba103e3a0b7cea192bb4aa52290d2
Агата <agata@MacBook-Pro-User.local>
2020-02-02 17:45:50 +0300

    init

---
+++ js/serov.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.sliders').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        
+    });
+  });
+  /*accessibility: true*/
